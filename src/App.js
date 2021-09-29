20e0e0ef79f61874c85be5f06cbdffdfcfd281dd
Carlos Quinteros <carlosn.quinteros@gmail.com>
2021-09-28 23:28:58 -0300

    Version inicial

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { Component } from "react";
+import Productos from "./components/Productos";
+import Layout from "./components/Layout";
+import Title from "./components/Title";
+import Navbar from "./components/Navbar";
+
+class App extends Component {
+  state = {
+    productos: [
+      {
+        name: "Tomate",
+        price: 1500,
+        img: "productos/tomate.jpg",
+      },
+      {
+        name: "Arbejas",
+        price: 2500,
+        img: "productos/arbejas.jpg",
+      },
+      {
+        name: "Lechuga",
+        price: 500,
+        img: "productos/lechuga.jpg",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarro={() => console.log("Agrega al carro")}
+            productos={this.state.productos}
+          />
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
